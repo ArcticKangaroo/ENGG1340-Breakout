6dfdb2fd0abf674676b4afa9b88703b344b3d7e3
tulsyanlakshya009 <127251539+tulsyanlakshya009@users.noreply.github.com>
2023-05-06 04:21:15 +0800

    Implement final menu (w/o credits)

--- menu.cpp
+++ menu.cpp
@@ -54,8 +54,9 @@ int mainmenu()
         {
             wattron(menuWindow, A_BOLD);//making all options bold
             if(i==highlight)
+            {
                 wattron(menuWindow, A_REVERSE);//highlighting the selected option
-            
+            }
             mvwprintw(menuWindow, 18+i+1, ((60-menu1[i].length())/2), menu1[i].c_str());//printing options
             wattroff(menuWindow, A_REVERSE);//de-highlighting the non selected options
         }
@@ -94,7 +95,7 @@ int mainmenu()
     }
     else if(highlight==2)
     {
-        highscore();
+        highscores();
     }
     else if(highlight==3)
     {
@@ -173,13 +174,12 @@ int inputName()
     game(playerName);
     return 0;
 }
+
 int instructions1()
 {
     WINDOW* instrucionWindow = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
     box(instrucionWindow, 0, 0);
     wrefresh(instrucionWindow);
-
-    string instructionmenu[2]={"<-BACK","NEXT->"};
     int choice;
     int highlight3=1;
     for(;;)
@@ -203,215 +203,22 @@ int instructions1()
         wattron(instrucionWindow, A_BOLD);//making all options bold
         mvwprintw(instrucionWindow, 24, 27, "PAGE 1");  
         //print file "instructions1.txt" here  
-        if(0==highlight3)
-        {
-            wattron(instrucionWindow, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucionWindow, 24, 1, instructionmenu[0].c_str());//printing options
-            wattroff(instrucionWindow, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucionWindow, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-        }
-        else if(1==highlight3)
-        {
-            wattron(instrucionWindow, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucionWindow, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-            wattroff(instrucionWindow, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucionWindow, 24, 1, instructionmenu[0].c_str());//printing options
-        }
+        wattron(instrucionWindow, A_REVERSE);//highlighting the selected option
+        mvwprintw(instrucionWindow, 24, 24, "BACK TO MENU");//printing options
+        wattroff(instrucionWindow, A_REVERSE);//de-highlighting the non selected options
         wrefresh(instrucionWindow);
-        
         choice = getch();//getting user input
-        if((choice==KEY_LEFT)||(choice==KEY_RIGHT))
-        {
-                if(highlight3==0)
-                    highlight3=1;
-                else
-                    highlight3=0;
-        }
         if(choice==10)
         {
             break;
         }
     }
-    if(highlight3==0)
-    {
-        mainmenu();
-    }
-    else
-    {
-        instructions2();
-    }
-    return 0;
-}
-
-int instructions2()
-{
-    WINDOW* instrucion2Window = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
-    box(instrucion2Window, 0, 0);
-    wrefresh(instrucion2Window);
-
-    string instructionmenu[2]={"<-BACK","NEXT->"};
-    int choice;
-    int highlight3=1;
-    for(;;)
-    {       
-        wattron(instrucion2Window, A_BOLD);//making all options bold
-        mvwprintw(instrucion2Window, 24, 27, "PAGE 2");
-        //print file "instructions2.txt" here
-        if(0==highlight3)
-        {
-            wattron(instrucion2Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion2Window, 24, 1, instructionmenu[0].c_str());//printing options
-            wattroff(instrucion2Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion2Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-        }
-        else if(1==highlight3)
-        {
-            wattron(instrucion2Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion2Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-            wattroff(instrucion2Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion2Window, 24, 1, instructionmenu[0].c_str());//printing options
-        }
-        wrefresh(instrucion2Window);
-        
-        choice = getch();//getting user input
-        if((choice==KEY_LEFT)||(choice==KEY_RIGHT))
-        {
-                if(highlight3==0)
-                    highlight3=1;
-                else
-                    highlight3=0;
-        }
-        if(choice==10)
-        {
-            break;
-        }
-    }
-    if(highlight3==0)
-    {
-        instructions1();
-    }
-    else
-    {
-        instructions3();
-    }
-    return 0;
-}
-int instructions3()
-{
-    WINDOW* instrucion3Window = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
-    box(instrucion3Window, 0, 0);
-    wrefresh(instrucion3Window);
-
-    string instructionmenu[2]={"<-BACK","NEXT->"};
-    int choice;
-    int highlight3=1;
-    for(;;)
-    {       
-        wattron(instrucion3Window, A_BOLD);//making all options bold
-        mvwprintw(instrucion3Window, 24, 27, "PAGE 3");
-        //print file "instructions3.txt" here
-        if(0==highlight3)
-        {
-            wattron(instrucion3Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion3Window, 24, 1, instructionmenu[0].c_str());//printing options
-            wattroff(instrucion3Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion3Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-        }
-        else if(1==highlight3)
-        {
-            wattron(instrucion3Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion3Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-            wattroff(instrucion3Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion3Window, 24, 1, instructionmenu[0].c_str());//printing options
-        }
-        wrefresh(instrucion3Window);
-        
-        choice = getch();//getting user input
-        if((choice==KEY_LEFT)||(choice==KEY_RIGHT))
-        {
-                if(highlight3==0)
-                    highlight3=1;
-                else
-                    highlight3=0;
-        }
-        if(choice==10)
-        {
-            break;
-        }
-    }
-    if(highlight3==0)
-    {
-        instructions2();
-    }
-    else
-    {
-        instructions4();
-    }
-    return 0;
-}
-
-int instructions4()
-{
-    WINDOW* instrucion4Window = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
-    box(instrucion4Window, 0, 0);
-    wrefresh(instrucion4Window);
-
-    string instructionmenu[2]={"<-BACK","END->"};
-    int choice;
-    int highlight3=1;
-    for(;;)
-    {       
-        wattron(instrucion4Window, A_BOLD);//making all options bold
-        mvwprintw(instrucion4Window, 24, 27, "PAGE 4");
-        //print file "instructions4.txt" here
-        if(0==highlight3)
-        {
-            wattron(instrucion4Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion4Window, 24, 1, instructionmenu[0].c_str());//printing options
-            wattroff(instrucion4Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion4Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-        }
-        else if(1==highlight3)
-        {
-            wattron(instrucion4Window, A_REVERSE);//highlighting the selected option
-            mvwprintw(instrucion4Window, 24, (59-instructionmenu[1].length()), instructionmenu[1].c_str());//printing options
-            wattroff(instrucion4Window, A_REVERSE);//de-highlighting the non selected options
-            mvwprintw(instrucion4Window, 24, 1, instructionmenu[0].c_str());//printing options
-        }
-        wrefresh(instrucion4Window);
-        
-        choice = getch();//getting user input
-        if((choice==KEY_LEFT)||(choice==KEY_RIGHT))
-        {
-                if(highlight3==0)
-                    highlight3=1;
-                else
-                    highlight3=0;
-        }
-        if(choice==10)
-        {
-            break;
-        }
-    }
-    if(highlight3==0)
-    {
-        instructions3();
-    }
-    else
-    {
-        mainmenu();
-    }
+    mainmenu();
     return 0;
 }
 
 int credits()
 {
-    return 0;
-}
-
-int highscore()
-{
-    highscores();
     return 0;
 }
 
