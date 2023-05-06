adfb29c19ad616d0640e7d78635c2b9cb74ad503
tulsyanlakshya009 <127251539+tulsyanlakshya009@users.noreply.github.com>
2023-05-06 17:06:04 +0800

    Added comments and in code documentation

--- menu.cpp
+++ menu.cpp
@@ -16,16 +16,20 @@ int credits();
 
 int mainmenu() {
 
-    WINDOW* menuWindow = newwin(26, 60, 0, 0); //main menu window initialization (rows,cols,y,x)
+    //main menu window initialization (rows,cols,y,x)
+    WINDOW* menuWindow = newwin(26, 60, 0, 0); 
     box(menuWindow, 0, 0);
     wrefresh(menuWindow);
 
+    //main menu options
     string menu1[5]= {"PLAY GAME","HOW TO PLAY","HIGH SCORE","CREDITS","QUIT"};
+    //variable to get user input
     int choice;
+    //variable to store the current option selected
     int highlight=0;
 
     while(TRUE) {
-        
+        //printing main menu screen
         mvwprintw(menuWindow, 1,2,"* * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
         mvwprintw(menuWindow, 2,2,"*   *   *   *   *   *   *   *   *   *   *   *    *      *");
         mvwprintw(menuWindow, 3,2,"*    .______* .______ * _______  *  ___   *  __ *___ *  *");
@@ -45,39 +49,45 @@ int mainmenu() {
         mvwprintw(menuWindow, 17,2,"* *  *   *  *   *    *     *    *    *    *   *  *  *  **"); 
         mvwprintw(menuWindow, 18,2,"* * * * * * * * * * * * * * * * * * * * * * * * * * * * *");  
         for(int i=0;i<5;i++) {
-            wattron(menuWindow, A_BOLD);//making all options bold
+            //making all options bold
+            wattron(menuWindow, A_BOLD);
+            //highlighting the selected choice
             if(i==highlight) {
-                wattron(menuWindow, A_REVERSE);//highlighting the selected option
+                wattron(menuWindow, A_REVERSE);
             }
+            //printing the choices
             mvwprintw(menuWindow, 18+i+1, ((60-menu1[i].length())/2), menu1[i].c_str());//printing options
             wattroff(menuWindow, A_REVERSE);//de-highlighting the non selected options
         }
-        choice = getch();//getting user input
-
+        //getting user input
+        choice = getch();
+        //changing highlighted option according to user option
         switch(choice) {
             case KEY_UP:
-                highlight--;//changing highlighted option
-                if(highlight<0)//checking whether the choice is going out of bounds
+                highlight--;
+                //checking whether the choice is going out of bounds
+                if(highlight<0)
                     highlight=4;
                 break;
             case KEY_DOWN:
-                highlight++;//changing highlighted option
-                if(highlight>4)//checking whether the choice is going out of bounds
+                highlight++;
+                //checking whether the choice is going out of bounds
+                if(highlight>4)
                     highlight=0;
                 break;
             default:
                 break;
         }
-
-        if(choice==10) { //checking for option selected
+        //checking whether enter is pressed
+        if(choice==10) { 
             break;
         }
-
+        //refresh window
         wrefresh(menuWindow);
     }
-
+    //traversing to the screen as per the user's choice
     if(highlight==0) {
-            inputName();
+        inputName();
     } else if(highlight==1) {
         instructions1();
     } else if(highlight==2) {
@@ -91,7 +101,7 @@ int mainmenu() {
 
 int inputName() {
 
-    // Create a 60x60 window
+    // input window initialization (rows, columns, y, x)
     WINDOW* win = newwin(26, 60, 0, 0);
 
     // Draw a box around the window
@@ -152,14 +162,17 @@ int inputName() {
         wrefresh(win);
         
     }
-    if(playerName.empty()) playerName = "NONAME";
-
+    //setting default name to "NONAME"
+    if(playerName.empty()) {
+        playerName = "NONAME";
+    }
     game(playerName);
     return 0;
 }
 
 int instructions1() {
-    WINDOW* instrucionWindow = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
+    //instruction window initialization (rows,cols,y,x)
+    WINDOW* instrucionWindow = newwin(26, 60, 0, 0); 
     box(instrucionWindow, 0, 0);
     wrefresh(instrucionWindow);
     int choice;
@@ -181,31 +194,39 @@ int instructions1() {
         mvwprintw(instrucionWindow, 16, 15, "If the ball drops down, you");
         mvwprintw(instrucionWindow, 17, 15, "lose a life.");
         mvwprintw(instrucionWindow, 20, 22, "Happy gaming!");
-        wattron(instrucionWindow, A_BOLD);//making all options bold 
-        //print file "instructions1.txt" here  
-        wattron(instrucionWindow, A_REVERSE);//highlighting the selected option
-        mvwprintw(instrucionWindow, 24, 45, "BACK TO MENU->");//printing options
-        wattroff(instrucionWindow, A_REVERSE);//de-highlighting the non selected options
+        //making all options bold 
+        wattron(instrucionWindow, A_BOLD);
+        //highlighting "BACK TO MENU" option
+        wattron(instrucionWindow, A_REVERSE);
+        //printing "BACK TO MENU->" option
+        mvwprintw(instrucionWindow, 24, 45, "BACK TO MENU->");
+        //switching off the highlight function
+        wattroff(instrucionWindow, A_REVERSE);
+        //refresh window
         wrefresh(instrucionWindow);
-        choice = getch();//getting user input
+        //getting user input
+        choice = getch();
+        //checking whether user pressed enter
         if(choice==10) {
             break;
         }
     }
+    //going back to main menu
     mainmenu();
     return 0;
 }
 
 int credits() {
-    WINDOW* endWindow = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
+    //credits window initialization (rows,cols,y,x)
+    WINDOW* endWindow = newwin(26, 60, 0, 0); 
     box(endWindow, 0, 0);
     wrefresh(endWindow);
 
-    int highlight=0;
+    //variable to get user input
     int choice;
 
     // Adding the names under the ASCII art
-    mvwprintw(endWindow, 13, 23, "Achal Agarwal");
+    mvwprintw(endWindow, 13, 24, "Achal Agarwal");
     mvwprintw(endWindow, 14, 23, " ");
     mvwprintw(endWindow, 15, 23, "Anubhav Kashyap");
     mvwprintw(endWindow, 16, 23, " ");
@@ -216,6 +237,7 @@ int credits() {
     mvwprintw(endWindow, 21, 23, "Lakshya Tulsyan");
 
     for(;;) {
+        //printing "Credits" on the screen
         mvwprintw(endWindow, 1,2,"* * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
         mvwprintw(endWindow, 2,2,"*   *   *   *   *   *   *   *   *   *   *   *   *   *   *");
         mvwprintw(endWindow, 3,2,"*  ________       *     *   *  *_   _*  __  *  * *      *");
@@ -228,34 +250,43 @@ int credits() {
         mvwprintw(endWindow, 10,2,"*     *     *     *     *     *     *     *     *    *  *");
         mvwprintw(endWindow, 11,2,"* * * * * * * * * * * * * * * * * * * * * * * ** * * *  *");
         
-        if(highlight==0) {
-            wattron(endWindow, A_BOLD);//making all options bold
-            wattron(endWindow,A_REVERSE);
-            mvwprintw(endWindow, 24, 24, "BACK TO HOME");
-            wattroff(endWindow,A_REVERSE);
-        }
+        // printing BACK TO HOME option
+        //making all options bold
+        wattron(endWindow, A_BOLD);
+        //highlighting BACK TO HOME option
+        wattron(endWindow,A_REVERSE);
+        mvwprintw(endWindow, 24, 24, "BACK TO HOME");
+        wattroff(endWindow,A_REVERSE);
+        //switching off highlight function
+        
+        //getting user input
         choice=getch();
+
+        //if user presses enter break
         if(choice==10) {
-            mainmenu();
-            return 0;
+            break;
         }
         wrefresh(endWindow);
     }
-
+    //going back to the main menu page
+    mainmenu();
     return 0;
 }
     
 
 int gameOver(string playerName, int playerScore) {
-    WINDOW* endWindow = newwin(26, 60, 0, 0); //instruction window initialization (rows,cols,y,x)
+    //gameover window initialization (rows,cols,y,x)
+    WINDOW* endWindow = newwin(26, 60, 0, 0); 
     box(endWindow, 0, 0);
     wrefresh(endWindow);
 
+    //sending player name and player score to store it in the file for high score screen
     InputToFile(playerName, playerScore);
 
-    int highlight=0;
+    //getting user input
     int choice;
     for(;;) {
+        //printing "Game Over" on the screen
         mvwprintw(endWindow, 1,2,"* * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
         mvwprintw(endWindow, 2,2,"*   *   *   *   *   *   *   *   *   *   *   *   *   *   *");
         mvwprintw(endWindow, 3,2,"       *    ______    *       *         *     *    *  *");
@@ -278,19 +309,24 @@ int gameOver(string playerName, int playerScore) {
         mvwprintw(endWindow, 20,2,"   *     *     *     *     *     *     *     *    *    *");
         mvwprintw(endWindow, 21,2,"* * * * * * * * * * * * * * * * * * * * * * * * * * * *");
         
-        if(highlight==0) {
-            wattron(endWindow, A_BOLD);//making all options bold
-            wattron(endWindow,A_REVERSE);
-            mvwprintw(endWindow, 24, 27, "BACK TO HOME");
-            wattroff(endWindow,A_REVERSE);
-        }
+        //making "BACK TO HOME" bold
+        wattron(endWindow, A_BOLD);
+        //highlighting "BACK TO HOME" option
+        wattron(endWindow,A_REVERSE);
+        mvwprintw(endWindow, 24, 27, "BACK TO HOME");
+        wattroff(endWindow,A_REVERSE);
+        //switching off the highlight function
+
+        //geting input from user
         choice=getch();
+
+        //checking whether the user pressed enter
         if(choice==10) {
-            mainmenu();
-            return 0;
+            break;
         }
+        //refreshing window
         wrefresh(endWindow);
     }
-
+    mainmenu();
     return 0;
 }
